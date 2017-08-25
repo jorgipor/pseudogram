ea7cbab1dd9f4e21f632ece0c8fdeb1ccec434c0
jordi <elgorje@gmail.com>
2017-08-25 15:55:27 +0200

    firebase and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  hableAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +22,7 @@ class App extends Component {
           <h2>Pseudogram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.hableAuth}>Login con Google</button>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyD8ChSIf91bNOUT66KJDebcTWKxw0J4j9k",
+    authDomain: "pseudogram-27eb0.firebaseapp.com",
+    databaseURL: "https://pseudogram-27eb0.firebaseio.com",
+    projectId: "pseudogram-27eb0",
+    storageBucket: "pseudogram-27eb0.appspot.com",
+    messagingSenderId: "481409389323"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
